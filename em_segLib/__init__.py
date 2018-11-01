bbcc9ec6a06c4f2b317631be68d3422f22d55255
matinraayai <raayaiardakani.m@husky.neu.edu>
2018-11-01 16:11:46 -0400

    Added seg malis dilation.

--- em_segLib/__init__.py
+++ em_segLib/__init__.py
@@ -1,7 +1,2 @@
-import io_util
-import aff_util
-import seg_util
-import seg_eval
-import seg_malis
-import seg_core
-import seg_dist
+from . import io_util, aff_util, seg_util, seg_eval, seg_malis, seg_core, \
+    seg_dist
--- em_segLib/seg_util.py
+++ em_segLib/seg_util.py
@@ -146,3 +146,25 @@ def fill_data(shape, filler={'type':'zero'}, dtype='float32'):
     else:
         raise RuntimeError('invalid filler type [%s]' % filler['type'])
     return data
+
+def genSegMalis(gg3,iter_num): # given input seg map, widen the seg border                           
+    from scipy.ndimage import morphology as skmorph                                                  
+    #from skimage import morphology as skmorph                                                       
+    gg3_dz = np.zeros(gg3.shape).astype(np.uint32)                                                   
+    gg3_dz[1:,:,:] = (np.diff(gg3,axis=0))                                                           
+    gg3_dy = np.zeros(gg3.shape).astype(np.uint32)                                                   
+    gg3_dy[:,1:,:] = (np.diff(gg3,axis=1))                                                           
+    gg3_dx = np.zeros(gg3.shape).astype(np.uint32)                                                   
+    gg3_dx[:,:,1:] = (np.diff(gg3,axis=2))                                                           
+                                                                                                     
+    gg3g = ((gg3_dx+gg3_dy)>0)                                                                       
+    #stel=np.array([[1, 1],[1,1]]).astype(bool)                                                      
+    #stel=np.array([[0, 1, 0],[1,1,1], [0,1,0]]).astype(bool)                                        
+    stel=np.array([[1, 1, 1],[1,1,1], [1,1,1]]).astype(bool)                                         
+    #stel=np.array([[1,1,1,1],[1, 1, 1, 1],[1,1,1,1],[1,1,1,1]]).astype(bool)                        
+    gg3gd=np.zeros(gg3g.shape)                                                                       
+    for i in range(gg3g.shape[0]):                                                                   
+        gg3gd[i,:,:]=skmorph.binary_dilation(gg3g[i,:,:],structure=stel,iterations=iter_num)         
+    out = gg3.copy()                                                                                 
+    out[gg3gd==1]=0                                                                                     #out[0,:,:]=0 # for malis                                                                            return out
+    return out
